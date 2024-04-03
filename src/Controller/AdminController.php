1ae0d4511bd3a1ef97cb35de40773941698525dc
erleur <flora.moreau@orange.fr>
2024-04-03 11:37:54 +0200

    admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+//    #[Route('/', name: 'app_admin')]
+//    public function index(): Response
+//    {
+//        return $this->render('admin/index.html.twig', [
+//            'controller_name' => 'AdminController',
+//        ]);
+//    }
+
+
+    #[Route('/liste', name:'app_admin_liste')]
+    public function listerUser(UserRepository $userRepository):Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('user/liste.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
+
+    #[Route('/new', name: 'app_admin_new', methods: ['GET', 'POST'])]
+    public function ajouterUtilisateur(Request $request, EntityManagerInterface $entityManager):Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+
+
+    #[Route('/{id}', name: 'app_admin_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+
 }
