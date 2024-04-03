58c7c9d95ad094ef348cd7e737b1ff82ae1a315d
erleur <flora.moreau@orange.fr>
2024-04-03 15:44:45 +0200

    enregistrement new user par admin

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,20 +4,40 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
+            ->add('email')
+//            ->add('roles')
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Merci d'entrer un mot de passe",
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
             ->add('isActif')
             ->add('isAdmin')
         ;
