e9c026be6fdca58fc85ba00a79ad22d6ff6c465d
SygVarys <131457897+SygVarys@users.noreply.github.com>
2024-04-04 08:57:10 +0200

    Faker + fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,28 +2,56 @@
 
 namespace App\DataFixtures;
 
+use App\DataFixtures\LieuFixtures;
 use App\Entity\Sortie;
+use App\Repository\LieuRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements DependentFixtureInterface
 {
+
+
+    private Faker\Generator $faker;
+    private LieuRepository $fakerLieu;
+
+
+    public function __construct(LieuRepository $lieuRepository)
+    {
+        $this->faker = Faker\Factory::create('fr_FR');
+        $this->fakerLieu = $lieuRepository;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $sortie = new Sortie();
+
         for($i=0; $i<10; $i++ ){
-            $sortie->setNom();
-            $sortie->setDateHeureDebut();
-            $sortie->setDuree();
-            $sortie->setDateLimiteInscription();
-            $sortie->setNbInscriptionsMax();
-            $sortie->setInfosSortie();
-            $sortie->setEtat();
-            $sortie->setLieu();
+            $sortie = new Sortie();
+            $sortie->setNom($this->faker->words(3, true));
+            $sec = strtotime($this->faker->date());
+            $date = date("d/m/y H:i", $sec);
+            $sortie->setDateHeureDebut($this->faker->dateTime());
+            $sortie->setDuree($this->faker->numerify('##'));
+            $sec = strtotime($this->faker->date());
+            $date = date("d/m/y H:i", $sec);
+            $sortie->setDateLimiteInscription($this->faker->dateTime());
+            $sortie->setNbInscriptionsMax($this->faker->randomDigit());
+            $sortie->setInfosSortie($this->faker->sentence());
+            $sortie->setEtat("En cours");
+            $sortie->setLieu($this->fakerLieu->find(3));
 
         }
         $manager->persist($sortie);
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            LieuFixtures::class,
+        ];
     }
 }
